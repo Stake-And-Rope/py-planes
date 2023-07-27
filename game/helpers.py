65ab3fc9f15a9b9d6e09a23f32fe1507521e9da4
Tamer <tamerchoyy@gmail.com>
2023-07-27 14:35:25 +0300

    added three functions

--- game/helpers.py
+++ game/helpers.py
@@ -6,3 +6,32 @@ def get_plane_speed(fps: int) -> float:
     }
 
     return plane_speed.get(fps)
+
+
+def get_background_roll_speed(fps: int) -> float:
+    looping_speed = {
+        45: 2,
+        60: 1.6,
+        75: 1.2,
+    }
+
+    return looping_speed.get(fps)
+
+
+def calculate_center(main_obj_width, child_obj_width):
+    """
+    run this code to visually see how it is being calculated
+    -- pygame.draw.rect(screen, (255, 0, 0), (window_size[0] // 2, 0, 1, window_size[1]), 1) --
+    -- pg    .draw.rect(screen, (255, 0, 0), (window_size[0] // 2, 0, 1, window_size[1]), 1) --
+
+    :return: int(pixels) of the X position where the child object must be placed
+    """
+    middle_of_main_obj = main_obj_width // 2
+    middle_of_child_obj = child_obj_width // 2
+
+
+    return middle_of_main_obj - middle_of_child_obj
+
+
+def get_screen_dimensions() -> tuple:
+    return 900, 800
