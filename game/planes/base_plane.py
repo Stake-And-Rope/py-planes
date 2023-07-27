5dd903fac830d436f2606cf684ab7e796bd01d4a
Tamer <tamerchoyy@gmail.com>
2023-07-27 14:34:02 +0300

    added borders and added extra checks for the spawn point

--- game/planes/base_plane.py
+++ game/planes/base_plane.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 import pygame as pg
-from abc import (ABC,
-                 abstractmethod,
-                 )
+
+from game.helpers import get_screen_dimensions
 
 
-class BasePlane(ABC):
+class Plane:
     LEFT_RIGHT_KEYS = ("a", "d")
     UP_DOWN_KEYS = ("w", "s")
 
@@ -15,9 +14,17 @@ class BasePlane(ABC):
                  fly_speed: float):
 
         self.model = pg.image.load(model).convert_alpha()
+        self.rect = self.model.get_rect()
+        self.height = self.rect.height
+        self.width = self.rect.width
         self.fly_speed = fly_speed
         self.x_pos = None
         self.y_pos = None
+
+    @property
+    def middle_screen_border(self):
+        height = get_screen_dimensions()[1]
+        return height // 2.5
 
     @property
     def get_plane_pos(self):
@@ -43,14 +50,58 @@ class BasePlane(ABC):
                         y_pos: int):
         """
         sets the x and y coordinates if the given args are integers
-        raises value error if one of the args is not integer
+        raises value error if one of the args is not integer or if the args are with invalid destination
         """
 
         if any(not isinstance(value, int) for value in (x_pos, y_pos)):
-            raise ValueError("Values must be integers")
+            raise ValueError("Values must be integers.")
+
+        if y_pos < self.middle_screen_border:
+            raise ValueError(f"y_pos cannot be between 0 and {self.middle_screen_border} pixels.")
+
+        screen_width, screen_height = get_screen_dimensions()
+
+        if y_pos > screen_height - self.height:
+            raise ValueError(f"y_pos must be between {self.middle_screen_border} and {screen_height - self.height} pixels.")
+
+
+        if x_pos > screen_width - self.width or x_pos < 0:
+            raise ValueError(f"x_pos must be between 0 and {screen_width - self.width} pixels.")
+
 
         self.x_pos = x_pos
         self.y_pos = y_pos
+
+    def display_plane(self, window):
+        window.blit(self.model, self.get_plane_pos)
+
+    def check_plane_boundaries(self):
+        """
+        this method checks the following borders:
+
+            1. left border (trying to get out of the screen from the left side)
+            2. right border (trying to get out of the screen from the right side)
+            3. bottom border (trying to get out of the screen from the bottom side)
+            4. middle border (trying to get above the invisible border in the middle)
+
+        if out of border, the plane will get returned in the border.
+        """
+        screen_width, screen_height = get_screen_dimensions()
+
+        right_border = screen_width - self.width
+        bottom_border = screen_height - self.height
+
+        if self.x_pos < 0:
+            self.x_pos = 0
+
+        elif self.x_pos > right_border:
+            self.x_pos = right_border
+
+        if self.y_pos < self.middle_screen_border:
+            self.y_pos = self.middle_screen_border
+
+        elif self.y_pos > bottom_border:
+            self.y_pos = bottom_border
 
     def plane_movement(self):
         """
@@ -64,3 +115,5 @@ class BasePlane(ABC):
 
             if button_press[button] and pg.key.name(button) in self.UP_DOWN_KEYS:
                 self.y_pos += self.move_directions[button]
+
+            self.check_plane_boundaries()
