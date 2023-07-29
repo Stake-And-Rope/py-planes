5297cfda0fb804955e8dd32343ca6fa99b642106
Tamer <tamerchoyy@gmail.com>
2023-07-29 23:32:32 +0300

    updated imports

--- game/bullets/bullet.py
+++ game/bullets/bullet.py
@@ -1,3 +1,7 @@
+from abc import (ABC,
+                 abstractmethod
+                 )
+
 from pygame import image
 
 from game.helpers import get_bullet_speed
@@ -8,8 +12,9 @@ fps = int(get_game_settings().get("fps"))
 bullet_speed = get_bullet_speed(fps)
 
 
-class Bullet:
+class Bullet(ABC):
     image = image.load("../images/user_plane_images/user_bullet.png").convert_alpha()
+    bullet_speed = get_bullet_speed(fps)
 
     def __init__(self, bullet_x: float, bullet_y: float):
         self.bullet_x = bullet_x
@@ -19,9 +24,6 @@ class Bullet:
     def get_bullet_pos(self):
         return self.bullet_x, self.bullet_y
 
+    @abstractmethod
     def move_bullet(self):
-        self.bullet_y -= bullet_speed
-
-
-
-
+        pass
