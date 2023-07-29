b5a47f3d384664aadc2932d4140ab985c5a09f32
Tamer <tamerchoyy@gmail.com>
2023-07-29 23:33:22 +0300

    added user bullet

---
+++ game/bullets/user_bullet.py
@@ -0,0 +1,10 @@
+from game.bullets.bullet import Bullet
+
+
+class UserBullet(Bullet):
+
+    def __init__(self, bullet_x: float, bullet_y: float):
+        super().__init__(bullet_x, bullet_y)
+
+    def move_bullet(self):
+        self.bullet_y -= self.bullet_speed
