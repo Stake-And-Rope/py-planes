7c482cf37d138e3467b4e0f4630f9ba7b7f56f66
Tamer <tamerchoyy@gmail.com>
2023-07-30 19:42:12 +0300

    changed imports

--- game/run_game.py
+++ game/run_game.py
@@ -8,37 +8,29 @@ from game.background_loop import BackgroundLoop
 from settings.settings_handler import get_game_settings
 from game import helpers
 
-
 window_size = helpers.get_screen_dimensions()
 screen = pg.display.set_mode(window_size)
 
-from game.planes.base_plane import Plane
-
+from game.planes.user_plane import UserPlane
 
 sys.path.append(r'..')
-
 
 user_settings = get_game_settings()
 
 fps = int(user_settings.get("fps"))
-plane_speed = helpers.get_plane_speed(fps)
 
-
-green_plane = Plane(
-    '../images/user_plane_images/user_plane_1.png',
-    plane_speed
-    )
+green_plane = UserPlane('../images/user_plane_images/user_plane_1.png')
 
 green_plane.set_spawn_point(
     helpers.calculate_center(window_size[0], green_plane.model.get_width()),
     650
     )
 
-
 background = BackgroundLoop(fps)
-
-
 clock = pg.time.Clock()
+
+SPAWN_ENEMY = pg.USEREVENT + 1
+pg.time.set_timer(SPAWN_ENEMY, 5000)
 
 running = True
 while running:
@@ -47,6 +39,9 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+
+        elif event.type == SPAWN_ENEMY:
+            print("Spawn enemy plane")
 
     background.loop_background(screen)
 
