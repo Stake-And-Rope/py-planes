6ffbf501acf3ed21ee27a294c73b53ad153ee7cb
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-08-02 13:57:44 +0300

    Changed some of the attributes' names and added an if condition or the methods "reduce_health_bar' and 'reduce_armor_bar'

--- game/bar.py
+++ game/bar.py
@@ -2,30 +2,30 @@ import pygame
 
 
 class Bar:
-    def __init__(self, x, y, width, height, max_health, first_colour, second_colour):
+    def __init__(self, x, y, width, height, max_health, top_colour, bottom_colour):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.max_health = max_health
         self.current_health = max_health
-        self.first_colour = first_colour
-        self.second_colour = second_colour
+        self.top_colour = top_colour
+        self.bottom_colour = bottom_colour
 
     def draw_bar(self, window):
         current_health_to_max_health = self.current_health / self.max_health
 
-        pygame.draw.rect(window, self.first_colour, (self.x, self.y, self.width, self.height))
-        pygame.draw.rect(window, self.second_colour, (self.x, self.y, current_health_to_max_health * self.width, self.height))
+        pygame.draw.rect(window, self.top_colour, (self.x, self.y, self.width, self.height), border_radius=3)
+        pygame.draw.rect(window, self.bottom_colour, (self.x, self.y, current_health_to_max_health * self.width, self.height), border_radius=3)
 
     def reduce_health_bar(self):
         keys_pressed = pygame.key.get_pressed()
 
-        if keys_pressed[pygame.K_1]:
+        if keys_pressed[pygame.K_1] and self.current_health:
             self.current_health -= 1
 
     def reduce_armour_bar(self):
         keys_pressed = pygame.key.get_pressed()
 
-        if keys_pressed[pygame.K_2]:
+        if keys_pressed[pygame.K_2] and self.current_health:
             self.current_health -= 1
