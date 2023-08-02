708b3e2e3c66c7016e9d100726833c57482bf7f8
Tamer <tamerchoyy@gmail.com>
2023-08-02 12:33:50 +0300

    update so we can visually see how the enemy plane is moving

--- game/run_game.py
+++ game/run_game.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import pygame as pg
+
 pg.init()
 
 import sys
@@ -10,8 +11,10 @@ from game import helpers
 
 window_size = helpers.get_screen_dimensions()
 screen = pg.display.set_mode(window_size)
+pg.display.set_caption("py-planes")
 
 from game.planes.user_plane import UserPlane
+from game.planes.enemy_plane import EnemyPlane
 
 sys.path.append(r'..')
 
@@ -20,7 +23,6 @@ user_settings = get_game_settings()
 fps = int(user_settings.get("fps"))
 
 green_plane = UserPlane('../images/user_plane_images/user_plane_1.png')
-
 green_plane.set_spawn_point(
     helpers.calculate_center(window_size[0], green_plane.model.get_width()),
     650
@@ -31,6 +33,9 @@ clock = pg.time.Clock()
 
 SPAWN_ENEMY = pg.USEREVENT + 1
 pg.time.set_timer(SPAWN_ENEMY, 5000)
+
+enemy = EnemyPlane("../images/enemy_plane_images/enemy_plane_1.png")
+enemy.set_spawn_point(400, -64)
 
 running = True
 while running:
@@ -44,6 +49,10 @@ while running:
             print("Spawn enemy plane")
 
     background.loop_background(screen)
+
+    enemy.plane_functionality()
+    enemy.display_plane(screen)
+    enemy.update_shot_bullets(screen)
 
     green_plane.plane_functionality()
     green_plane.display_plane(screen)
