4576f32d1db6940727d72ab17375046871c44150
Tamer <tamerchoyy@gmail.com>
2023-08-02 15:28:24 +0300

    added the enemy controller calls

--- game/run_game.py
+++ game/run_game.py
@@ -15,6 +15,7 @@ pg.display.set_caption("py-planes")
 
 from game.planes.user_plane import UserPlane
 from game.planes.enemy_plane import EnemyPlane
+from game.planes.control_enemies import EnemyController
 from bar import Bar
 
 sys.path.append(r'..')
@@ -23,7 +24,7 @@ user_settings = get_game_settings()
 
 fps = int(user_settings.get("fps"))
 
-green_plane = UserPlane('../images/user_plane_images/user_plane_1.png')
+green_plane = UserPlane(pg.image.load('../images/user_plane_images/user_plane_1.png').convert_alpha())
 green_plane.set_spawn_point(
     helpers.calculate_center(window_size[0], green_plane.model.get_width()),
     650
@@ -32,11 +33,14 @@ green_plane.set_spawn_point(
 background = BackgroundLoop(fps)
 clock = pg.time.Clock()
 
-SPAWN_ENEMY = pg.USEREVENT + 1
-pg.time.set_timer(SPAWN_ENEMY, 5000)
+SPAWN_ENEMY_PLANE = pg.USEREVENT + 1
+pg.time.set_timer(SPAWN_ENEMY_PLANE, 5000)
 
-enemy = EnemyPlane("../images/enemy_plane_images/enemy_plane_1.png")
-enemy.set_spawn_point(400, -64)
+enemy_planes_surface_objects = [
+    pg.image.load(f"../images/enemy_plane_images/enemy_plane_{i}.png").convert_alpha() for i in range(1, 4)
+]
+
+enemies = EnemyController()
 
 health_bar = Bar(500, 760, 145, 25, 100, (110, 137, 181), (21, 43, 79))  # last two tuples are rgb colour codes
 armour_bar = Bar(740, 760, 145, 25, 100, (110, 137, 181), (21, 43, 79))  # last two tuples are rgb colour codes
@@ -49,8 +53,9 @@ while running:
         if event.type == pg.QUIT:
             running = False
 
-        elif event.type == SPAWN_ENEMY:
-            print("Spawn enemy plane")
+        elif event.type == SPAWN_ENEMY_PLANE:
+            random_image = helpers.get_random_image_of_enemy_planes(enemy_planes_surface_objects)
+            enemies.add_enemy(EnemyPlane(random_image))
 
     background.loop_background(screen)
 
@@ -60,9 +65,7 @@ while running:
     armour_bar.reduce_armour_bar()
     armour_bar.draw_bar(screen)
 
-    enemy.plane_functionality()
-    enemy.display_plane(screen)
-    enemy.update_shot_bullets(screen)
+    enemies.update_enemies(screen)
 
     green_plane.plane_functionality()
     green_plane.display_plane(screen)
