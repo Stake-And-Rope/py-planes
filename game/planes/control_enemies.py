06508b768067ccb8b6e5d90f1c8826bce1c37424
Tamer <tamerchoyy@gmail.com>
2023-08-04 19:12:25 +0300

    added logic for the towers

--- game/planes/control_enemies.py
+++ game/planes/control_enemies.py
@@ -1,20 +1,32 @@
 from game.helpers import random_enemy_plane_coordinates
 from game.planes.enemy_plane import EnemyPlane
+from game.planes.enemy_tower import Tower
 
 
 class EnemyController:
 
     def __init__(self):
-        self.enemies = []
+        self.enemy_planes = []
+        self.towers = []
 
     def add_enemy(self, enemy):
+        random_x_spawn = random_enemy_plane_coordinates()
+        enemy.set_spawn_point(*random_x_spawn)
+
         if isinstance(enemy, EnemyPlane):
-            enemy.set_spawn_point(*random_enemy_plane_coordinates())
+            self.enemy_planes.append(enemy)
 
-        self.enemies.append(enemy)
+        elif isinstance(enemy, Tower):
+            self.towers.append(enemy)
 
-    def update_enemies(self, screen):
-        for enemy in self.enemies:
-            enemy.plane_functionality()
-            enemy.display_plane(screen)
-            enemy.update_shot_bullets(screen)
+    def update_planes(self, screen):
+        for plane in self.enemy_planes:
+            plane.functionality()
+            plane.display_plane(screen)
+            plane.update_shot_bullets(screen)
+
+    def update_towers(self, screen, user_plane):
+        for tower in self.towers:
+            tower.display_plane(screen)
+            tower.functionality(screen, user_plane)
+            tower.update_shot_bullets(screen)
