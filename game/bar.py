27ba9d386f626058f0f0349a90c9469ad95c59ec
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2023-08-12 09:19:13 +0300

    Bars' drawing logic changed
    - When the hide_bar_when_empty is False the bar will not be hidden when it is empty. If you set the optional argument as True the bar will hide when it becomes empty.

--- game/bar.py
+++ game/bar.py
@@ -6,20 +6,22 @@ class Bar:
         self.x = x
         self.y = y
         self.width = width
+        self.current_width = width
         self.height = height
         self.max_value = max_value
         self.current_value = max_value
         self.top_colour = top_colour
         self.bottom_colour = bottom_colour
 
-    def draw_bar(self, window):
-        current_width = self.lower_upper_bar_width()
-
+    def draw_bar(self, window, hide_bar_when_empty=False):
+        if self.current_value <= 0 and hide_bar_when_empty:
+            return
         pygame.draw.rect(window, self.top_colour, (self.x, self.y, self.width, self.height), border_radius=3)
-        pygame.draw.rect(window, self.bottom_colour, (self.x, self.y, current_width, self.height), border_radius=3)
+        pygame.draw.rect(window, self.bottom_colour, (self.x, self.y, self.current_width, self.height), border_radius=3)
 
     def reduce_bar(self, damage_value):
         self.current_value -= damage_value
+        self.lower_upper_bar_width()
 
     def increase_bar(self, increase_value):
         self.current_value += increase_value
@@ -28,6 +30,6 @@ class Bar:
         result_width = (self.current_value / self.max_value) * self.width
 
         if result_width <= 0:
-            return 0
-
-        return result_width
+            self.current_width = 0
+        else:
+            self.current_width = result_width
