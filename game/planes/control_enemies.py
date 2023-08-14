8953b1b0ebcd7294c11d3409f9b1821dfbe28af3
Tamer <tamerchoyy@gmail.com>
2023-08-14 20:30:45 +0300

    now creating health and armor bars on every enemy spawn. Added bar drawing in the update enemy methods

--- game/planes/control_enemies.py
+++ game/planes/control_enemies.py
@@ -12,6 +12,8 @@ class EnemyController:
     def add_enemy(self, enemy):
         random_x_spawn = random_enemy_plane_coordinates()
         enemy.set_spawn_point(*random_x_spawn)
+        enemy.create_health_bar()
+        enemy.create_armor_bar()
 
         if isinstance(enemy, EnemyPlane):
             self.enemy_planes.append(enemy)
@@ -22,11 +24,13 @@ class EnemyController:
     def update_planes(self, screen):
         for plane in self.enemy_planes:
             plane.functionality()
+            plane.health_bar.draw_bar(window=screen, hide_bar_when_empty=True)
             plane.display_plane(screen)
             plane.update_shot_bullets(screen)
 
     def update_towers(self, screen, user_plane):
         for tower in self.towers:
+            tower.health_bar.draw_bar(window=screen, hide_bar_when_empty=True)
             tower.display_plane(screen)
             tower.functionality(screen, user_plane)
             tower.update_shot_bullets(screen)
